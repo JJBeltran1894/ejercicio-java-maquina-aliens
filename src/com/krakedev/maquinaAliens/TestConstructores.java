d6af4dd412b190d67a3f672bce1e9f98e3c11250
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-04-18 18:02:12 -0500

    JJBA: Se elimina la clase module-info.java - Se agrega la clase  de pruebas JUnit - Se modifica la clase TestConstructores

--- src/com/krakedev/maquinaAliens/TestConstructores.java
+++ src/com/krakedev/maquinaAliens/TestConstructores.java
@@ -3,7 +3,7 @@ package com.krakedev.maquinaAliens;
 public class TestConstructores {
 
 	public static void main(String[] args) {
-		Alien alien1=new Alien(25,"rojo");
+		Alien alien1=new Alien(26,"rojo");
 		Alien alien2=new Alien(60,"verde");
 		Alien alien3=new Alien(2,"azul");
 		
